e7b4700e75e17d127e222790377a7e235e53aa8a
yerlibilgin <myildiz83@gmail.com>
2016-03-11 08:32:37 +0200

    init method added, main deprecated

--- src/main/scala/gov/tubitak/minder/client/MinderClientApp.scala
+++ src/main/scala/gov/tubitak/minder/client/MinderClientApp.scala
@@ -1,14 +1,32 @@
 package gov.tubitak.minder.client
 
+import minderengine.Wrapper
 import org.apache.log4j.PropertyConfigurator
 
 /**
  * The entry point for a minder client application.
  */
 object MinderClientApp {
+
+  /**
+    * Initialize the adapter mechanism
+    *
+    * this is deprecated please use init()
+ *
+    * @param args
+    */
+  @Deprecated
   def main(args: Array[String]) {
+  }
+
+
+  /**
+    * Initialize the adapter mechanism and return the created adapter.
+    */
+  def init(): Wrapper ={
     PropertyConfigurator.configure(classOf[MinderClient].getResource("/logging.properties"))
     val minderClient = new MinderClient
+    minderClient.wrapper
   }
 
 }
